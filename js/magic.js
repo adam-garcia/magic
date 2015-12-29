8fecce361bc5e7e75e2ba49f2bbd6f01a067682b
adam <aegarcia@bu.edu>
2015-12-28 21:10:50 -0500

    idk fell asleep before commit

--- js/magic.js
+++ js/magic.js
@@ -3,19 +3,30 @@
 var cards = ["#one", "#two", "#three", "#four", "#five", "#six"];
 var card;
 var guess;
+
+function sum (array) {
+    var tot = 0;
+    if (array.length == 1) {
+        return array[0];
+    } else {
+        for (var i = array.length - 1; i >= 0; i--) {
+            tot += array[i];
+        };
+        return tot;
+    }
+}
+
 function beginGame () {
     $('#game').toggleClass('hide');
     $('#intro').toggleClass('hide');
     card = 0;
-    guess = 0;
+    guess = [];
 }
 function select (onCard) {
     // Handle card value
     if (onCard == 'yes') {
-        console.log('aaa');
-        guess = guess + Math.pow(2, card);
-    } else {
-        console.log('bbb')
+        // guess = guess + Math.pow(2, card);
+        guess.push(Math.pow(2, card));
     }
     // Final card
     if (card == 5) {
@@ -31,8 +42,21 @@ function nextCard () {
     $(cards[card]).toggleClass('hide');
 }
 
+function lastCard () {
+    if (card != 0) {
+        $(cards[card]).toggleClass('hide');
+        card -= 1;
+        $(cards[card]).toggleClass('hide');
+    } else {
+        beginGame();
+    }
+}
+
 function guessCard () {
-    if (guess == 0) { guess = 1 }
-    alert('Was your number ' + guess + '?');
-    location.reload();
+    if (length.guess == 0) { 
+        alert('Was your number ' + 1 + '?');
+    } else {
+        alert('Was your number ' + sum(guess) + '?');
+        location.reload();
+    }
 }
